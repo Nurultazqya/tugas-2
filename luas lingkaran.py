bef3e7822e200082a1355613655209274930e6e5
Nurultazqya <ntazqyah@gmail.com>
2024-09-14 14:49:58 +0700

    Create luas lingkaran.py

---
+++ luas lingkaran.py
@@ -0,0 +1,10 @@
+import math
+
+# Mengambil input dari pengguna
+jari_jari = float(input("Masukkan jari-jari lingkaran: "))
+
+# Menghitung luas lingkaran
+luas = math.pi * (jari_jari ** 2)
+
+# Menampilkan hasil
+print("Luas lingkaran adalah:", luas)
