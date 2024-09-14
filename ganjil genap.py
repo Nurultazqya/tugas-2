6c34826356ea191d34001ea74ea18cd4e444d729
Nurultazqya <ntazqyah@gmail.com>
2024-09-14 14:51:12 +0700

    Create ganjil genap.py

---
+++ ganjil genap.py
@@ -0,0 +1,8 @@
+# Mengambil input dari pengguna
+bilangan = int(input("Masukkan bilangan: "))
+
+# Menentukan apakah bilangan genap atau ganjil
+if bilangan % 2 == 0:
+    print("Bilangan tersebut adalah genap.")
+else:
+    print("Bilangan tersebut adalah ganjil.")
