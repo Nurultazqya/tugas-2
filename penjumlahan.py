1eb3148c596363481d1e7e613d83766a57b87cbd
Nurultazqya <ntazqyah@gmail.com>
2024-09-14 14:48:31 +0700

    Create penjumlahan.py

---
+++ penjumlahan.py
@@ -0,0 +1,9 @@
+# Mengambil input dari pengguna
+bilangan1 = float(input("Masukkan bilangan pertama: "))
+bilangan2 = float(input("Masukkan bilangan kedua: "))
+
+# Menjumlahkan kedua bilangan
+hasil = bilangan1 + bilangan2
+
+# Menampilkan hasil
+print("Hasil penjumlahan:", hasil)
